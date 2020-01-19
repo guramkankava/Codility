3242a3754ced3af9bb8b248fe8215d0f649a31e1
guramkankava <g.kankava@yahoo.com>
2020-01-19 15:00:24 +0400

    Odd occurrenceInArraytask solution

---
+++ src/main/java/ge/gngapps/OddOccurrencesInArray.java
@@ -0,0 +1,52 @@
+package ge.gngapps;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OddOccurrencesInArray {
+
+    public static void main(String[] args) {
+        int [] source = new int [] {1,2,3,1,3,1,1};
+        int [] source2 = new int [] {9, 3, 9, 3, 9, 7, 9};
+        System.out.println(solutionByBinary(source));
+    }
+
+    public static void solution(int[] source) {
+
+    }
+
+    public static Integer solutionByList(int[] source) {
+        List<Integer> result = new ArrayList<>();
+        for (int element : source) {
+            int index;
+            if((index = result.indexOf(element)) >= 0) {
+                result.remove(index);
+            } else {
+                result.add(element);
+            }
+        }
+        return result.get(0); 
+    }
+
+    public static Integer solutionByMap(int[] source) {
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int element : source) {
+            if(result.containsKey(element)) {
+                result.remove(element);
+            } else {
+                result.put(element, element);
+            }
+        }
+        return new ArrayList<Integer>(result.values()).get(0);
+    }
+
+    public static Integer solutionByBinary(int[] source) {
+        int result = source[0];
+        for (int index = 1; index < source.length; index++) {
+            result = result ^ source[index]; 
+        }
+        return result;
+    }
+}
